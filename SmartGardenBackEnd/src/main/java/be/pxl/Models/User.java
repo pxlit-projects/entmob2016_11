e6012c87dad7792511e74d540e5a362fc1d4af29
ToonFroeyen <toon.froeyen@student.pxl.be>
2016-10-30 16:56:32 +0100

    Verandering generation type

--- SmartGardenBackEnd/src/main/java/be/pxl/Models/User.java
+++ SmartGardenBackEnd/src/main/java/be/pxl/Models/User.java
@@ -20,7 +20,7 @@ public class User {
         this.password = password;
     }
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     @Column(name="id")
     private int id;
 
